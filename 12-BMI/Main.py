d7765ee5db956cb39b731cd07364a6fec3a6fcb1
muthia07 <ainiyyatia@gmail.com>
2024-12-21 22:30:42 +1200

    Delete 12-BMI directory

--- 12-BMI/Main.py
+++
@@ -1,7 +0,0 @@
-tinggi_badan = float(input("Masukkan tinggi badan :"))
-berat_badan = float (input("Masukkan berat badan :"))
-
-tinggi_badan = tinggi_badan / 100 
-bmi = berat_badan / (tinggi_badan ** 2)
-
-print(f"BMI adalah : {bmi:.1f}")
